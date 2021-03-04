fa3d597d5cae157e41d3518936d8df4809d76038
Lennart Vandoorne <lennrtv@gmail.com>
2021-03-04 18:23:39 +0100

    [feat] Timeslot overview page

--- src/app/components/timeslot-overview/timeslot-overview.component.ts
+++ src/app/components/timeslot-overview/timeslot-overview.component.ts
@@ -1,4 +1,10 @@
+import { Timeslot } from './../../models/timeslot';
+import { Project } from './../../models/project';
+import { TimeslotsService } from './../../services/timeslots.service';
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { switchMap } from 'rxjs/operators';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-timeslot-overview',
@@ -7,9 +13,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimeslotOverviewComponent implements OnInit {
 
-  constructor() { }
+  public projects: Project[];
+  public timeslots: Timeslot[];
+
+  public today: Date = new Date();
+  public moment = moment;
+
+  public projectSelect: FormControl = new FormControl('');
+  public startDate: FormControl = new FormControl(moment().startOf('isoWeek').format('YYYY-MM-DD'));
+  public endDate: FormControl = new FormControl(moment().endOf('isoWeek').format('YYYY-MM-DD'));
+
+  constructor(
+    private timeslotService: TimeslotsService
+  ) { }
 
   ngOnInit(): void {
+    this.timeslotService.getProjects().subscribe(projects => {
+      this.projects = projects;
+      this.onProjectChange();
+      this.onPeriodChange();
+    });
   }
 
+  /**
+   * Show new project timeslots when current project is changed
+   */
+  private onProjectChange() {
+    this.projectSelect.valueChanges.pipe(
+      switchMap(projectId => {
+        return this.timeslotService.getTimeslots(projectId);
+      })
+    ).subscribe(timeslots => {
+      this.timeslots = timeslots;
+      console.log('timeslots: ', this.timeslots);
+   })
+  }
+
+  /**
+   * Re-render timeslots when time period changes
+   */
+  private onPeriodChange() {
+    this.startDate.valueChanges.subscribe(d => {
+      console.log('start date: ', this.startDate.value);
+      if(this.timeslots?.length > 0){this.timeslots = [...this.timeslots]};
+   });
+    this.endDate.valueChanges.subscribe(d => {
+      console.log('end date: ', this.endDate.value);
+      if(this.timeslots?.length > 0){this.timeslots = [...this.timeslots]};
+   });
+  }
+
+  public toDate(dateString: string, type: string): Date {
+    console.log(type + ' date: ', new Date(dateString));
+    return new Date(dateString);
+  }
 }
